062f1109160e09559be3c1665f22ca0b2d9b25fb
ryanboris <ryan.boris@gmail.com>
2019-01-12 16:42:02 -0500

    ugh

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-console.log('App.js is running!');
+console.log('App.js is running!')
 
 // JSX - JavaScript XML
-var template = <h1>Indecision App</h1>;
-var appRoot = document.getElementById('app');
+var template = <h1>Indecision App</h1>
+var appRoot = document.getElementById('app')
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(template, appRoot)
